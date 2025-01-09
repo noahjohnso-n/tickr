aedcbff90bf4293555d89c5a5f39298fc6aae752
Noah Johnson <noahjohnson@Noahs-MacBook-Air.local>
2025-01-09 10:43:37 -0500

    Database adjustments

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ import passport from "passport";
 import { Strategy } from "passport-local";
 import GoogleStrategy from "passport-google-oauth2"
 import env from "dotenv";
+import { createClient } from "@supabase/supabase-js";
 
 
 const app = express();
