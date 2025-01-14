3531a2752b1fb3ce7934b7d22d13df99438a07e9
Noah Johnson <noahjohnson@Noahs-MacBook-Air.local>
2025-01-14 13:38:37 -0500

    Database try 19

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ import passport from "passport";
 import { Strategy } from "passport-local";
 import GoogleStrategy from "passport-google-oauth2"
 import env from "dotenv";
+import pkg from 'pg';
+const { Pool } = pkg;
 
 
 const app = express();
