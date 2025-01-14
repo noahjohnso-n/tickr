0eed6e7700b51efc60e3719c85b3939519accc42
Noah Johnson <noahjohnson@Noahs-MacBook-Air.local>
2025-01-13 21:59:15 -0500

    Database try 2

--- index.js
+++ index.js
@@ -41,9 +41,9 @@ const db = new pg.Client({
     // database: process.env.DB_DATABASE,
     // password: process.env.DB_PASSWORD,
     // port: process.env.DB_PORT,
-    // ssl:{
-    //     rejectUnauthorized: false
-    // }
+    ssl:{
+        rejectUnauthorized: false
+    }
 });
   
   db.connect();
