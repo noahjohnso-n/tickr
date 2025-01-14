e4e009cf5b18e573822ae98f8d76defbc3b79968
Noah Johnson <noahjohnson@Noahs-MacBook-Air.local>
2025-01-14 10:11:41 -0500

    Database try 12

--- index.js
+++ index.js
@@ -35,11 +35,16 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 const db = new pg.Client({
-    connectionString: process.env.DATABASE_URL,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    port: process.env.DB_PORT,
+    host: process.env.DB_HOST,
+    ssl: true,
     connectionTimeoutMillis: 10000,  // Increase the timeout
-    ssl:{
-        rejectUnauthorized: false
-    }
+    // ssl:{
+    //     rejectUnauthorized: false
+    // }
 });
   
   db.connect()
