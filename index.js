187838807e1b1b555a99a416e174e88e7b9c01b1
Noah Johnson <noahjohnson@Noahs-MacBook-Air.local>
2025-01-16 09:24:35 -0500

    Try 6

--- index.js
+++ index.js
@@ -35,11 +35,6 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 const db = new pg.Client({
-    // user: process.env.DB_USER,
-    // host: process.env.DB_HOST,
-    // database: process.env.DB_NAME,
-    // password: process.env.DB_PASSWORD,
-    // port: process.env.DB_PORT,
     connectionString: process.env.DB_URL,
     ssl: { rejectUnauthorized: false} 
 });
